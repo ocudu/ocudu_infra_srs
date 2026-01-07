75a47573eec71b670f34980369e498bf57a796ab
Alfredo Sáez <alfredo.saez@srs.io>
2026-01-07 09:57:44 +0100

    all: update copyright header after new year

--- e2e/scripts/run_e2e_pipeline.py
+++ e2e/scripts/run_e2e_pipeline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Copyright 2021-2025 Software Radio Systems Limited
+# Copyright 2021-2026 Software Radio Systems Limited
 #
 # By using this file, you agree to the terms and conditions set
 # forth in the LICENSE file which can be found at the top level of
