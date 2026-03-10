cde1a9cf9a823f70c4684be6b12a35ff9663bb21
Ismael Gomez <ismael@srs.io>
2026-03-10 18:35:16 +0100

    add 512UE ping test
    Signed-off-by: Ismael Gomez <ismael@srs.io>

--- retina/agent/src/retina/agent/templates/amarisoft_mme_tun.sh
+++ retina/agent/src/retina/agent/templates/amarisoft_mme_tun.sh
@@ -48,8 +48,8 @@ if [ "$type" = "ipv4" ] ; then
     echo '1' > /proc/sys/net/ipv6/conf/"$ifname"/disable_ipv6 2>/dev/null || sysctl -w net.ipv6.conf."$ifname".disable_ipv6=1 2>/dev/null || true
 
     # run ltesim_server
-    echo "*** Running ltesim_server for $ifaddr:3000"
-    /opt/ltemme/ltesim_server -a $ifaddr:3000 &
+    echo "*** Running ltesim_server for $tun_ip_prefix.$first_subnet.1:3000"
+    /opt/ltemme/ltesim_server -a $tun_ip_prefix.$first_subnet.1:3000 &
 else
 
     ll="$5"     # ipv6 link local address
