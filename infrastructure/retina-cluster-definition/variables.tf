1fbbbd2f22ceb7e61d58f76b8b8002c48fad416c
Alfredo Sáez <alfredo.saez@srs.io>
2026-03-16 13:00:45 +0100

    iac: update license header
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- infrastructure/retina-cluster-definition/variables.tf
+++ infrastructure/retina-cluster-definition/variables.tf
@@ -1,10 +1,5 @@
-#
-# Copyright 2021-2026 Software Radio Systems Limited
-#
-# By using this file, you agree to the terms and conditions set
-# forth in the LICENSE file which can be found at the top level of
-# the distribution.
-#
+# SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+# SPDX-License-Identifier: BSD-3-Clause-Open-MPI
 
 variable "cluster_config_path" {
   description = "Path to the cluster YAML file (absolute, or relative to the consumer root directory)."
