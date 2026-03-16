9b199f30e4eee0f78da5e604d2029a648b567628
Alfredo Sáez <alfredo.saez@srs.io>
2026-03-16 12:52:46 +0100

    iac: clean up files from old approach
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- infrastructure/k8s/core/tf/coredump/variables.tf
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
-# SPDX-License-Identifier: BSD-3-Clause-Open-MPI
-
-variable "namespace" {
-  type        = string
-  description = "Kubernetes namespace for the coredump-cleanup DaemonSet"
-  default     = "infra"
-}
