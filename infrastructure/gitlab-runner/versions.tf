2fd620fbeb93b0a4befeebfbb15d0b827bbc5fff
Alfredo Sáez <alfredo.saez@srs.io>
2026-03-23 15:57:26 +0100

    ci: tf modules static check
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

---
+++ infrastructure/gitlab-runner/versions.tf
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+# SPDX-License-Identifier: BSD-3-Clause-Open-MPI
+
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
---
+++ infrastructure/retina-cluster-setup/versions.tf
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+# SPDX-License-Identifier: BSD-3-Clause-Open-MPI
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+  }
+}
