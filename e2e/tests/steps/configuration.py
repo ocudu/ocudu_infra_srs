09e64a70a54f8fbfe83463362ff03353912e496e
Alfredo Sáez <alfredo.saez@srs.io>
2026-04-27 18:50:03 +0200

    e2e: fix no warning allowlist
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- e2e/tests/steps/configuration.py
+++ e2e/tests/steps/configuration.py
@@ -265,7 +265,7 @@ def configure_test_parameters(
                 "pucch_formats": pucch_formats,
                 "pdsch_interleaving_bundle_size": pdsch_interleaving_bundle_size,
                 "slices": slices if slices is not None else [],
-                "warning_allowlist": warning_allowlist,
+                "warning_allowlist": warning_allowlist if warning_allowlist is not None else [],
             },
         },
         "du": {
