e893d7c007da27cae05d7ab6d9eaf5e89b0e32b8
Alfredo Sáez <alfredo.saez@srs.io>
2026-06-24 14:03:03 +0000

    retina: upgrade mongodb for open5gs
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- retina/images/open5gs/install_open5gs_dependencies.sh
+++ retina/images/open5gs/install_open5gs_dependencies.sh
@@ -8,16 +8,12 @@ set -e
 
 . /etc/os-release
 
-# Workaround until repo is available for ubuntu 24.04
-if [[ "$UBUNTU_CODENAME" == "noble" ]]; then
-    UBUNTU_CODENAME=jammy
-fi
-
 # mongdb
 DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y --no-install-recommends ca-certificates wget gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | apt-key add
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $UBUNTU_CODENAME/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y --no-install-recommends mongodb-org &&
+wget -qO - https://www.mongodb.org/static/pgp/server-8.0.asc | apt-key add
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $UBUNTU_CODENAME/mongodb-org/8.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y --no-install-recommends \
+    mongodb-org-server mongodb-org-shell mongodb-mongosh &&
     apt-get autoremove && apt-get clean
 
 # open5g
