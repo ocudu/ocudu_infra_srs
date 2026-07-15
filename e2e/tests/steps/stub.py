4f8c780095ec937dc8aafb774868829d8b8e23b4
Alfredo Sáez <alfredo.saez@srs.io>
2026-07-15 09:15:29 +0000

    e2e: increase ocudu apps startup time
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- e2e/tests/steps/stub.py
+++ e2e/tests/steps/stub.py
@@ -55,7 +55,7 @@ from retina.protocol.ue_pb2_grpc import UEStub
 RF_MAX_TIMEOUT: int = 5 * 60  # Time enough in RF when loading a new image in the sdr
 UE_STARTUP_TIMEOUT: int = RF_MAX_TIMEOUT
 GNB_STARTUP_TIMEOUT: int = (
-    15  # GNB delay (we wait x seconds and check it's still alive). UE later and has a big timeout
+    45  # GNB delay (we wait x seconds and check it's still alive). UE later and has a big timeout
 )
 FIVEGC_STARTUP_TIMEOUT: int = RF_MAX_TIMEOUT
 ATTACH_TIMEOUT: int = 90
