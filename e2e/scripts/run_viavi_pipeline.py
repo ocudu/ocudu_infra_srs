6e53eaebc037f49bc119016afa6e6d4bb86d1729
Alfredo Sáez <alfredo.saez@srs.io>
2026-09-15 11:19:16 +0000

    ci,e2e: upgrade dpdk version
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- e2e/scripts/run_viavi_pipeline.py
+++ e2e/scripts/run_viavi_pipeline.py
@@ -66,7 +66,7 @@ BUILD_DEFINITIONS: Dict[str, _BuildDefinition] = {
         target="gnb_split_7_2",
         build_args="-DCMAKE_BUILD_TYPE=Release -DFORCE_DEBUG_INFO=True -DENABLE_UHD=False -DENABLE_DPDK=True "
         '-DENABLE_ZEROMQ=False -DENABLE_FFTW=False -DENABLE_MKL=True -DMARCH="x86-64-v4"',
-        dpdk_version="25.11_avx512",
+        dpdk_version="25.11.3_avx512",
         uhd_version="",
     ),
     "rtsan": _BuildDefinition(
@@ -77,7 +77,7 @@ BUILD_DEFINITIONS: Dict[str, _BuildDefinition] = {
         build_args="-DCMAKE_BUILD_TYPE=Release -DFORCE_DEBUG_INFO=True -DENABLE_UHD=False -DENABLE_DPDK=True "
         '-DENABLE_ZEROMQ=False -DENABLE_FFTW=False -DENABLE_MKL=True -DMARCH="x86-64-v4" '
         "-DENABLE_RTSAN=True -DENABLE_WERROR=False",
-        dpdk_version="25.11_avx512",
+        dpdk_version="25.11.3_avx512",
         uhd_version="",
     ),
 }
